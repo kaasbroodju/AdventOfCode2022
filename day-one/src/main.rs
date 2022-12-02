84102b011f8a20c68179b90dedcd2d1ee7099ffe
kaasbroodju <morriswaaijer8@gmail.com>
2022-12-02 11:09:49 +0100

    day two

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -1,6 +1,10 @@
-mod day_one;
+extern crate core;
 
-use day_one::*;
+mod day_one;
+mod day_two;
+
+// change this use for different days
+use day_two::*;
 
 fn main() {
     println!("first part: {:?}", first_part());
--- day-one/src/my_benchmark.rs
+++ day-one/src/my_benchmark.rs
@@ -1,13 +1,21 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 mod day_one;
+mod day_two;
 use day_one::*;
+use day_two::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut day_one_group = c.benchmark_group("day one");
-    day_one_group.bench_function("first part", |b| b.iter(|| first_part()));
-    day_one_group.bench_function("second part", |b| b.iter(|| second_part()));
+    day_one_group.bench_function("first part", |b| b.iter(|| day_one::first_part()));
+    day_one_group.bench_function("second part", |b| b.iter(|| day_one::second_part()));
 
-    // c.bench_function("fib 20", |b| b.iter(|| first_part()));
+    day_one_group.finish();
+
+    let mut day_two_group = c.benchmark_group("day two");
+    day_two_group.bench_function("first part", |b| b.iter(|| day_two::first_part()));
+    day_two_group.bench_function("second part", |b| b.iter(|| day_two::second_part()));
+
+    day_two_group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
