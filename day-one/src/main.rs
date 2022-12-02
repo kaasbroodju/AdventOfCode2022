9260f9c96468209e25c39138a6df06a1fba7aeaf
kaasbroodju <morriswaaijer8@gmail.com>
2022-12-02 13:24:41 +0100

    refactor renaming files

--- day-one/src/main.rs
+++ day-one/src/main.rs
@@ -1,10 +1,10 @@
 extern crate core;
 
-mod day_one;
-mod day_two;
+mod day_01;
+mod day_02;
 
 // change this use for different days
-use day_two::*;
+use day_02::*;
 
 fn main() {
     println!("first part: {:?}", first_part());
--- day-one/src/my_benchmark.rs
+++ day-one/src/my_benchmark.rs
@@ -1,19 +1,19 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-mod day_one;
-mod day_two;
-use day_one::*;
-use day_two::*;
+mod day_01;
+mod day_02;
+use day_01::*;
+use day_02::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut day_one_group = c.benchmark_group("day one");
-    day_one_group.bench_function("first part", |b| b.iter(|| day_one::first_part()));
-    day_one_group.bench_function("second part", |b| b.iter(|| day_one::second_part()));
+    day_one_group.bench_function("first part", |b| b.iter(|| day_01::first_part()));
+    day_one_group.bench_function("second part", |b| b.iter(|| day_01::second_part()));
 
     day_one_group.finish();
 
     let mut day_two_group = c.benchmark_group("day two");
-    day_two_group.bench_function("first part", |b| b.iter(|| day_two::first_part()));
-    day_two_group.bench_function("second part", |b| b.iter(|| day_two::second_part()));
+    day_two_group.bench_function("first part", |b| b.iter(|| day_02::first_part()));
+    day_two_group.bench_function("second part", |b| b.iter(|| day_02::second_part()));
 
     day_two_group.finish();
 }
